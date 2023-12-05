937d1a232db83aa39bf29902d1bb34580a13d5b9
crocchi <ziocro@gmail.com>
2023-12-05 18:03:53 +0100

    update

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -5,7 +5,8 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const { Server } = require("socket.io");
 const cors = require('cors');
-const router = require('./routers')
+const router = require('./routers');
+const { userLogout,userLogin } = require(".-/event/socketEventHandlers");
 
 
 class Main{
@@ -35,29 +36,48 @@ class Main{
         this.server = http.createServer(app);
         this.io = new Server(server);
 
-        // STARTTING THE SERVER
+        // STARTING THE SERVER
         this.server.listen(PORT);
         console.log(`Server is listening on port ${PORT}`);
 
+        //SOCKET.IO EVENT HANDLER
+        this.onConnection = (socket) => {
 
+            socket.data.username = (socket.id).slice(1, 6);
+            const user = socket.data.username
+
+            userLogin(socket.data.username);
+
+            socket.on("disconnect", userLogout);
+            //socket.on("order:read", readOrder);
+            //socket.on("user:update-password", updatePassword);
+          }
+
+        this.io.on("connection", this.onConnection);
+        
+        /*
+        this.io.on('connection', async (socket) => {    
+
+            
+
+            socket.on('disconnect', () => {
+                logger.info(`${user} disconnected!`);
+                this.io.emit('chat message', { 
+                    msg:`${user} left the party`, 
+                    username: user, 
+                    isSystemMessage:true, 
+                    timestamp: null
+                });
+            });
+
+        })*/
+    }  
+
+    close() {
+        this.server.close()
+        this.io.close()
     }
-    
-
-    
 }
-
-//EXPRESS CONF
-
-
-// index page
-app.get('/', function (req, res) {
-    res.render('pages/index', {
-     // msg: req.rawHeaders
-    });
-  });
-
-
-
 
 
 
--- BACKEND/routers/index.js
+++ BACKEND/routers/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Server is running...');
-});
+ // res.render('pages/index', { msg: req.rawHeaders }) ;});
+
+})
 
 module.exports = router;
