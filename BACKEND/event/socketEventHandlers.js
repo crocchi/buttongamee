e460fb69d1ce2d48bf2a2d1f4cc56b126e87acff
crocchi <ziocro@gmail.com>
2023-12-05 17:58:23 +0100

    Update socketEventHandlers.js

--- BACKEND/event/socketEventHandlers.js
+++ BACKEND/event/socketEventHandlers.js
@@ -1,1 +1,20 @@
+module.exports = (io) => {
+  const userLogout = function (payload) {
+    const socket = this; // hence the 'function' above, as an arrow function will not work
+     io.emit('chat message', { 
+                    msg:`${socket.data.username} left the party`, 
+                    username:socket.data.username, 
+                    isSystemMessage:true
+                });
+    // ...
+  };
 
+  const userLogin = function (orderId, callback) {
+    // ...
+  };
+
+  return {
+    userLogout,
+    userLogin
+  }
+}
